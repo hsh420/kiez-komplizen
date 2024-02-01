bbcad3f3366077bd66193dfa7a6cd1e52e164f6e
Katharina-Beck <katja131291@gmail.com>
2024-02-01 13:29:44 +0100

    add: button to pages and styled in css

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -2,16 +2,18 @@
   <h1>Login</h1>
   <form @submit.prevent="handleSubmit">
     <input name="email" type="email" placeholder="E-Mail" v-model="store.email" />
-    <label for="email">E-Mail</label>
+    <label for="email">E-Mail</label><br>
     <input name="password" type="password" placeholder="Passwort" v-model="store.password" />
     <label for="password">Passwort</label>
-    <button type="submit">Login</button>
+    <ButtonGlobal :label="'Login'"/>
   </form>
   <p>...oder mit Google anmelden</p>
-  <button v-on:click="store.loginWithGoogle">Login with Google</button>
+  <!-- <img class="icon-google" alt="Google Logo" src="@/assets/pictures-layout/google-icon.png" > -->
+  <button class="google-button" v-on:click="store.loginWithGoogle">Login with Google</button>
 </template>
 
 <script setup>
+import ButtonGlobal from '@/components/ButtonGlobal.vue';
 import { useAuthStore } from '@/stores/auth'
 
 const store = useAuthStore()
@@ -19,3 +21,28 @@ const handleSubmit = () => {
   store.login()
 }
 </script>
+
+
+<style scoped>
+.google-button {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  width: 358px;
+  height: 54px;
+  margin: 10px 0 20px 0;
+  position: relative;
+  top: 200px;
+  left: 36px;
+  border-radius: 50px;
+  border: 2px solid;
+  border-color: var(--color-neutral-n200);
+  font-family: 'SourceSansPro-Semibold', sans-serif;
+  font-size: 14px;
+  font-weight: 500;
+}
+button:hover {
+  background-color: lightgray;
+}
+
+</style>
