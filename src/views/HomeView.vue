440d3a3c080e3458f94938d78fec5721280aac6c
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2024-02-02 15:14:57 +0100

    resove merge changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,11 +1,9 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
 import { useAuthStore } from '@/stores/auth'
 const store = useAuthStore()
 </script>
 
 <template>
-  <HeaderLayout />
   <main>
     <button v-on:click="store.logout()">Logout</button>
     <RouterLink to="/settings">Einstellungen</RouterLink>
@@ -13,5 +11,4 @@ const store = useAuthStore()
     <RouterLink to="/offer">Anbieten</RouterLink>
     <RouterLink to="/messages">Nachrichten</RouterLink>
   </main>
-  <FooterLayout />
 </template>
