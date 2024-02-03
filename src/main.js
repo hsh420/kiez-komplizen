8858e5f7ddef1cd606b994a205a00fdfdc421d3c
Katharina-Beck <katja131291@gmail.com>
2024-02-03 13:53:06 +0100

    fix: file names change

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import './assets/main.css'
-import '@/components/ButtonGlobal.vue'
+import '@/components/ButtonGlobalComponent.vue'
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
 
--- src/views/RegistrationView.vue
+++ src/views/RegistrationView.vue
@@ -2,20 +2,19 @@
   <h1>Account erstellen</h1>
   <form @submit.prevent="handleSubmit">
     <input name="firstname" type="text" placeholder="Vorname" v-model="store.firstname" />
-    <label for="firstname">Vorname</label><br>
+    <label for="firstname">Vorname</label><br />
     <input name="lastname" type="text" placeholder="Nachname" v-model="store.lastname" />
-    <label for="lastname">Nachname</label><br>
+    <label for="lastname">Nachname</label><br />
     <input name="email" type="email" placeholder="E-Mail" v-model="store.email" />
-    <label for="email">E-Mail</label><br>
+    <label for="email">E-Mail</label><br />
     <input name="password" type="password" placeholder="Passwort" v-model="store.password" />
     <label for="password">Passwort</label>
-    <ButtonGlobal :label="'Registration'"/>
+    <ButtonGlobal :label="'Registration'" />
   </form>
-
 </template>
 
 <script setup>
-import ButtonGlobal from '@/components/ButtonGlobal.vue';
+import ButtonGlobal from '@/components/ButtonGlobalComponent.vue'
 import { useAuthStore } from '@/stores/auth'
 
 const store = useAuthStore()
