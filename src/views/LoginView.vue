d0e115f15f8a55bee4a4c0e66d4d9b8b5fc67fb0
Katharina-Beck <katja131291@gmail.com>
2024-02-03 18:12:01 +0100

    add: styling input and label for LoginView

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,18 +1,32 @@
 <template>
   <HamburgerNavComponent />
-  <h1>Login</h1>
-  <form @submit.prevent="handleSubmit">
-    <input name="email" type="email" placeholder="E-Mail" v-model="store.email" />
-    <label for="email">E-Mail</label><br />
-    <input name="password" type="password" placeholder="Passwort" v-model="store.password" />
-    <label for="password">Passwort</label>
+  <main>
+    <h1>Login</h1>
+    <form @submit.prevent="handleSubmit">
+      <div class="input-container">
+        <label for="inputField1"
+          >E-Mail<br />
+          <input type="text" id="inputField1" placeholder="E-Mail eingeben" v-model="store.email"
+        /></label>
+      </div>
+      <div class="input-container">
+        <label for="password"
+          >Passwort<br />
+          <input
+            type="text"
+            id="inputField2"
+            placeholder="Passwort eingeben"
+            v-model="store.password"
+        /></label>
+      </div>
+    </form>
     <ButtonGlobal :label="'Login'" type="submit" class="login-button" />
-  </form>
-  <p>...oder mit Google anmelden</p>
-  <button class="google-button" v-on:click="store.loginWithGoogle">
-    <img class="icon-google" alt="Google Logo" src="@/assets/pictures-layout/google_icon.png" />
-    &nbsp; Login with Google
-  </button>
+    <p>...oder mit Google anmelden</p>
+    <button class="google-button" v-on:click="store.loginWithGoogle">
+      <img class="icon-google" alt="Google Logo" src="@/assets/pictures-layout/google_icon.png" />
+      &nbsp; Login with Google
+    </button>
+  </main>
   <FooterNavComponent />
 </template>
 
@@ -29,28 +43,33 @@ const handleSubmit = () => {
 </script>
 
 <style scoped>
-h1 {
-  text-align: center;
+main {
+  overflow: ;
 }
 
-form {
-  display: flex;
-  flex-direction: column;
-  justify-content: center;
-  margin-bottom: 10px;
-  padding: 5px;
+p {
+  text-align: center;
+  margin-top: 60px;
 }
+
+h1 {
+  text-align: center;
+  margin-top: 160px;
+}
+
 .login-button {
   margin: 0 auto;
   width: 80%;
+  margin-top: 10px;
 }
+
 .google-button {
   display: flex;
   align-items: center;
   justify-content: center;
   width: 80%;
   height: 54px;
-  margin: 10px 0 20px 0;
+  /* margin: 10px 0 20px 0; */
   position: relative;
   border-radius: 50px;
   border: 2px solid;
@@ -59,8 +78,31 @@ form {
   font-size: 14px;
   font-weight: 500;
   margin: 0 auto;
+  margin-top: 10px;
 }
 button:hover {
   background-color: lightgray;
+}
+
+.input-container {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin-bottom: auto;
+}
+
+.input-container label {
+  margin-bottom: 50px;
+  display: block;
+}
+
+.input-container input[type='text'] {
+  width: 300px;
+  height: 40px;
+  font-size: 16px;
+  padding: 10px;
+  border: 1px solid lightgray;
+  border-radius: 5px;
 }
 </style>
