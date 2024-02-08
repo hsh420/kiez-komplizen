24f3989bd98d0032d24e3c9589a159d920efdf71
Jenny <147312222+jennywhitmore@users.noreply.github.com>
2024-02-08 15:12:31 +0100

    add stzling to the search fields and minor h1 h2 changes on all pages

--- src/views/MessagesView.vue
+++ src/views/MessagesView.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>Messages works!</p>
+  <h1>Komplizen Kontakte</h1>
+  <h2>Hier kannst du sie knüpfen</h2>
+  <hr />
+  <br />
+  <p>Wir arbeiten dran!</p>
 </template>
--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,3 +1,7 @@
 <template>
-  <p>Settings works!</p>
+  <h1>Einstellungen</h1>
+  <h2>Hier kannst du bissl was rumschrauben</h2>
+  <hr />
+  <br />
+  <p>Wir arbeiten dran!</p>
 </template>
